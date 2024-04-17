b8cd6f681e43e618c1a27fb2868cc071b39d785e
rohinis <rohinis@altair.com>
2024-04-17 18:42:18 +0530

    001-1
    001-1

--- startTest.sh
+++ startTest.sh
@@ -4,7 +4,7 @@
 echo "==========================================================================="
 echo "================= Started Job Submission Test Case  ======================="
 echo "==========================================================================="
-./katalonc -noSplash -runMode=console -projectPath="/tmp/AccessKRE/AccessKRE.prj" -retry=0 -testSuitePath="Test Suites/Sanity_new/JobSubmission" -browserType="Firefox (headless)" -executionProfile="ExecProfile-raju" -apiKey="c7396a10-aa94-4865-8337-8e396199dfbc" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true
+./katalonc -noSplash -runMode=console -projectPath="/tmp/AccessWebCICD/AccessWeb.prj" -retry=0 -testSuitePath="Test Suites/Sanity_new/JobSubmission" -browserType="Firefox (headless)" -executionProfile="ExecProfile-raju" -apiKey="c7396a10-aa94-4865-8337-8e396199dfbc" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true
 
 echo "==========================================================================="
 echo "=================  Ended Job Submission Test Case   ======================="
