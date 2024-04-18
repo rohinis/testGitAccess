6e2f0a7441af9209ad14ad4d1906bccab7d92c0e
rohinis <rohinis@altair.com>
2024-04-18 19:25:03 +0530

    005-003
    005-003

---
+++ Test Suites/Sanity_new/CI-CD-Set1.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>CI-CD-Set1</name>
+   <tag></tag>
+   <delayBetweenInstances>4</delayBetweenInstances>
+   <executionMode>PARALLEL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>ExecProfile-david</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Firefox (headless)</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Sanity_new/FilesModule-WithPreReq</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>ExecProfile-raju</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Firefox (headless)</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Sanity_new/ProfileModule-WithPreReq</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>ExecProfile-serviceuser</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Firefox (headless)</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Sanity_new/FolderModule-WithPreReq</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
--- startTest.sh
+++ startTest.sh
@@ -2,10 +2,26 @@
 ## c7396a10-aa94-4865-8337-8e396199dfbc ##
 
 echo "==========================================================================="
-echo "================= Started Job Submission Test Case  ======================="
+echo "========================= Started SET - 1   ==============================="
 echo "==========================================================================="
-./katalonc -noSplash -runMode=console -projectPath="/tmp/AccessWebCICD/AccessWeb.prj" -retry=0 -testSuitePath="Test Suites/Sanity_new/JobSubmission" -browserType="Firefox (headless)" -executionProfile="ExecProfile-raju" -apiKey="c7396a10-aa94-4865-8337-8e396199dfbc" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true
+
+
+./katalonc -noSplash -runMode=console -projectPath="/tmp/AccessWebCICD/AccessWeb.prj" -retry=0 -testSuiteCollectionPath="Test Suites/Sanity_new/CI-CD-Set1" -apiKey="c7396a10-aa94-4865-8337-8e396199dfbc" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true
 
 echo "==========================================================================="
-echo "=================  Ended Job Submission Test Case   ======================="
+echo "=========================   DONE SET - 1    ==============================="
 echo "==========================================================================="
+echo "###########################################################################"
+echo "==========================================================================="
+echo "========================= Started SET - 2   ==============================="
+echo "==========================================================================="
+
+
+./katalonc -noSplash -runMode=console -projectPath="/tmp/AccessWebCICD/AccessWeb.prj" -retry=0 -testSuiteCollectionPath="Test Suites/Sanity_new/CI-CD-Set2" -apiKey="c7396a10-aa94-4865-8337-8e396199dfbc" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true
+
+echo "==========================================================================="
+echo "=========================   DONE SET - 2    ==============================="
+echo "==========================================================================="
+
+
+
