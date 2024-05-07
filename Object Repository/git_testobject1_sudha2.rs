ad31be56bf7c4b298c0f4afd5863ff89ed49e7fe
sudharani-TK <42799724+sudharani-TK@users.noreply.github.com>
2024-05-07 12:31:11 +0530

    new Test Object2

--- Object Repository/git_testobject1_sudha2.rs
+++ Object Repository/git_testobject1_sudha2.rs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>git_testobject1_sudha</name>
+   <name>git_testobject1_sudha2</name>
    <tag></tag>
    <elementGuidId>6e6e8dd7-3cb2-4f8e-a964-e0d454ac358d</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
